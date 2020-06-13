b5b924ebe5e61f945fb21c4da7321b39bb5c4186
brandon252mann <64870171+brandon252mann@users.noreply.github.com>
2020-06-13 08:42:09 -0400

    Add files via upload

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import constants from '../constants'
+import { TurboClient } from '../utils'
+
+
+export default {
+
+	currentUserReceived: (user) => {
+		return {
+			type: 'CURRENT_USER_RECEIVED',
+			data: user
+		}
+	}
+	
+}
---
+++ src/components/containers/Admin.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react'
+import superagent from 'superagent'
+import { connect } from 'react-redux'
+import actions from '../../actions'
+
+class Admin extends Component {
+
+	constructor(){
+		super()
+		this.state = {
+
+		}
+	}
+
+	componentDidMount(){
+		superagent.get('/auth/currentuser')
+		.query(null)
+		.set('Accept', 'application/json')
+		.end((err, response) => {
+			if (err){
+				console.log('REQUEST ERROR: ' + err.message)
+				return
+			}
+
+			const currentuser = response.body.user
+			if (currentuser == null){
+				console.log('USER NOT LOGGED IN: ')
+				return
+			}
+
+			console.log('USER: ' + JSON.stringify(currentuser))
+			this.props.currentUserReceived(currentuser)
+		})
+	}
+
+	updateUsername(event){
+		console.log('updateUsername: ' + event.target.value)
+		this.setState({
+			username: event.target.value
+		})
+	}
+
+	updateUser(event){
+		event.preventDefault()
+		console.log('Update User!')
+		if (this.state.username == null){
+			alert('No Changes Made!')
+			return
+		}
+
+		superagent.post('/auth/update')
+		.send({username: this.state.username})
+		.set('Accept', 'application/json')
+		.end((err, response) => {
+			if (err){
+				alert('Error: ' + err.message)
+				return
+			}
+
+			console.log('User Updated: ' + JSON.stringify(response.body))
+			const currentUser = response.body.user
+			this.props.currentUserReceived(currentUser)
+
+			alert('User Updated!')
+		})
+	}
+
+	render(){
+		const currentUser = this.props.user.currentUser
+		let username = ''
+
+		if (currentUser == null)
+			console.log('NO CURRENT USER!')
+		else {
+			username = currentUser.username
+			console.log('CURRENT USER IS: ' + JSON.stringify(currentUser))
+		}
+
+		return (
+			<div className="container">
+				<h1>Welcome {username}</h1>
+				<hr />
+
+				{ (currentUser == null) ? null : (
+						<form>
+							<input type="text" onChange={this.updateUsername.bind(this)} defaultValue={currentUser.username} placeholder="Username" />
+							<br />
+							<button onClick={this.updateUser.bind(this)}>Update User</button>
+						</form>
+					)
+				}
+			</div>
+
+		)
+	}
+}
+
+const stateToProps = (state) => {
+	return {
+		user: state.user
+	}
+}
+
+const dispatchToProps = (dispatch) => {
+	return {
+		currentUserReceived: (user) => dispatch(actions.currentUserReceived(user))
+
+	}
+}
+
+export default connect(stateToProps, dispatchToProps)(Admin)
+
+
+
+
